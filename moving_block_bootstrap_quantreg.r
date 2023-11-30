aafe560fda855cb56eb87beb972dee1449336cf7
KAI-LAN CHANG <thekylan@gmail.com>
2023-11-30 16:30:55 -0700

    Update moving_block_bootstrap_quantreg.r

--- moving_block_bootstrap_quantreg.r
+++ moving_block_bootstrap_quantreg.r
@@ -26,8 +26,11 @@ mbfun=function(formula,data,tau){
 }
 
 set.seed(2013)
-fit=coef(rq(yd~x, data=mlo, tau=0.5))*12 #intercept and slope
-op=t(replicate(1000, mbfun(formula=yd~x,data=mlo,tau=0.5)))
+formula=yd~x
+##note: one can directly use formula=y~x+sin(2*pi*month/12)+cos(2*pi*month/12)+sin(2*pi*month/6)+cos(2*pi*month/6)
+##if data are not deseasonalized in the previous step
+fit=coef(rq(formula=formula, data=mlo, tau=0.5))*12 #intercept and slope
+op=t(replicate(1000, mbfun(formula=formula,data=mlo,tau=0.5)))
 fit_se=apply(op, 2, sd, na.rm=TRUE)*12 #MBB standard error for intercept and slope
 fit_pv=2*pt(q=abs(fit/fit_se), df=nrow(mlo)-2, lower.tail=FALSE) #MBB p value for intercept and slop
 #multiple quantiles 
